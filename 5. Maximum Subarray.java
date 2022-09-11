bea4b59770556ffaba3350a95c5a635fa01b0c26
Ranjan Goyal <ranjangoyal98@gmail.com>
2022-09-11 15:21:34 -0400

    Update 5. Maximum Subarray.java

--- 5. Maximum Subarray.java
+++ 5. Maximum Subarray.java
@@ -15,3 +15,20 @@ class Solution {
         return max;
     }
 }
+
+// Not required to worry about length == 1
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int current = 0;
+        for(int i=0; i<nums.length; i++){
+            current += nums[i];
+            max = Math.max(max, current);
+            if(current<0){
+                current = 0;
+            }
+        }
+        return max;
+    }
+}
